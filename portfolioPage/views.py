b25c9b84e0872d4656cdf293f0377fec875d7213
Doomzy <mohamed_abdulhamid5@outlook.com>
2021-04-24 21:35:47 +0200

    Made the projects dynamic

--- portfolioPage/views.py
+++ portfolioPage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import owner
+from .models import owner,project
 
 def mainPage(request):
+    projects= project.objects.all()
     ownerInfo= owner.objects.get()
-    return render(request, "portfolio.html" ,{"owner":ownerInfo})
+    return render(request, "portfolio.html" ,{"owner":ownerInfo , "projects":projects})
