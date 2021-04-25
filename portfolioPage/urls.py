6412ca60368c3519355e42c3f92672a5f6d1427c
Doomzy <mohamed_abdulhamid5@outlook.com>
2021-04-25 23:40:14 +0200

    Created the 'Blog' page

--- portfolioPage/urls.py
+++ portfolioPage/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
-name= "portfolioPage"
+app_name= "portfolioPage"
 
 urlpatterns =[
-    path('', views.mainPage)
+    path('', views.mainPage, name="portfolio")
 ]
 
-urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
